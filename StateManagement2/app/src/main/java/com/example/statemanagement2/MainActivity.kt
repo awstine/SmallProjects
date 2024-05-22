4ccf331d4a6aad6bd15431f81a18e1d7feb2a22c
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-05-22 14:40:42 -0700

    initial commit

--- StateManagement2/app/src/main/java/com/example/statemanagement2/MainActivity.kt
+++ StateManagement2/app/src/main/java/com/example/statemanagement2/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -86,6 +87,11 @@ fun TipTimeLayout() {
             label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it },
+            keyboardOptions =
+                KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next,
+                ),
             modifier =
                 Modifier
                     .padding(bottom = 32.dp)
@@ -96,6 +102,10 @@ fun TipTimeLayout() {
             label = R.string.tip_percentage,
             value = tipInput,
             onValueChange = { tipInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             modifier =
                 Modifier
                     .padding(bottom = 32.dp)
@@ -123,6 +133,7 @@ private fun calculateTip(
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
     value: String,
     onValueChange: (String) -> Unit = {},
     modifier: Modifier = Modifier,
@@ -133,6 +144,6 @@ fun EditNumberField(
         onValueChange = onValueChange,
         modifier = modifier,
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
     )
 }
