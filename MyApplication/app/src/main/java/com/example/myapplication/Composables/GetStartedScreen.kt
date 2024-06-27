117765cbf39de3074127e70e4677c0111ee651ba
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-06-27 03:13:14 -0700

    initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/Composables/GetStartedScreen.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Composables/GetStartedScreen.kt
@@ -6,17 +6,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.myapplication.R
@@ -25,38 +33,60 @@ import com.example.myapplication.data.Screen
 @Composable
 @Suppress("ktlint:standard:function-naming")
 fun GetStartedScreen(navController: NavController) {
-    Surface {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground,
+    ) {
         Column(
             modifier =
                 Modifier
                     .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background),
-            verticalArrangement = Arrangement.Bottom, // Center the content vertically
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(16.dp),
+            verticalArrangement = Arrangement.Center, // Center the content vertically
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Image(
                 modifier =
                     Modifier
                         .fillMaxWidth(0.8f)
-                        .padding(bottom = 140.dp),
+                        .padding(bottom = 32.dp),
+                // Adjusted padding for a better layout
                 painter = painterResource(id = R.drawable.saloon),
                 contentDescription = null,
             )
             Text(
                 "Welcome to Our Saloon App",
                 style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Center, // Center align text
                 modifier = Modifier.padding(16.dp),
             )
-            Button(
+            Spacer(modifier = Modifier.height(16.dp))
+            ElevatedButton(
                 onClick = {
-                    navController.navigate(Screen.LogInScreen.route)
+                    navController.navigate(Screen.HomeScreen.route)
                 },
                 modifier =
                     Modifier
                         .fillMaxWidth(0.8f) // 80% of available width
-                        .padding(vertical = 16.dp),
+                        .padding(vertical = 16.dp)
+                        .size(60.dp),
+                elevation =
+                    ButtonDefaults.elevatedButtonElevation(
+                        defaultElevation = 6.dp,
+                    ),
+                shape = RoundedCornerShape(35.dp),
+                colors =
+                    ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.tertiary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
             ) {
-                Text(text = "Get Started")
+                Text(
+                    text = "Get Started",
+                    style = MaterialTheme.typography.headlineSmall,
+                    textAlign = TextAlign.Center, // Center align text
+                )
             }
         }
     }
