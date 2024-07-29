c042b909b5533876199bd43e4135714954ff66fb
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-07-29 08:19:41 -0700

    initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/Composables/StylistsScreen.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Composables/StylistsScreen.kt
@@ -2,6 +2,9 @@
 
 package com.example.myapplication.Composables
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,22 +17,30 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,6 +52,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myapplication.data3.stylistList
+import java.sql.Date
+import java.text.SimpleDateFormat
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Suppress("ktlint:standard:function-naming")
@@ -121,7 +134,7 @@ fun StylistsScreen(
 
                             Spacer(modifier = Modifier.height(8.dp))
 
-                            DatePicker()
+                            DialogBox()
                         }
                         Surface(
                             shape = RoundedCornerShape(16.dp),
@@ -140,37 +153,108 @@ fun StylistsScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Suppress("ktlint:standard:function-naming")
 @Composable
-private fun DatePicker() {
-    val book by remember { mutableStateOf("Book") }
-    val openDialog = remember { mutableStateOf(true) }
+@Preview
+private fun DialogBox() {
+    val openDialogOne = remember { mutableStateOf(false) }
+    val text by remember { mutableStateOf("Book") }
+    var dateResult by remember { mutableStateOf("Date Picker") }
+    OutlinedButton(
+        onClick = { openDialogOne.value = true },
+        shape = RoundedCornerShape(35.dp),
+        colors =
+            ButtonDefaults.outlinedButtonColors(
+                contentColor = MaterialTheme.colorScheme.primary,
+            ),
+        modifier = Modifier.size(width = 100.dp, height = 40.dp),
+        elevation =
+            ButtonDefaults.elevation(
+                defaultElevation = 8.dp,
+                pressedElevation = 0.dp,
+                disabledElevation = 0.dp,
+            ),
+    ) {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            style = MaterialTheme.typography.titleLarge,
+        )
+    }
+    if (openDialogOne.value) { // Check state of the dialog
+        AlertDialog(
+            modifier = Modifier.padding(12.dp),
+            onDismissRequest = { openDialogOne.value = true },
+            title = { Text(text = " My Dialog title") },
+            text = { Text(text = "Are you sure you want exit") },
+            confirmButton = {
+                TextButton(onClick = { openDialogOne.value = false }) {
+                    Text(text = "Confirm")
+                }
+            },
+            shape = RoundedCornerShape(30.dp),
+            dismissButton = {
+                TextButton(onClick = { openDialogOne.value = false }) {
+                    Text(text = "Cancel")
+                }
+            },
+        )
+    }
 
-    Column {
-        OutlinedButton(
-            onClick = { openDialog.value = true },
-            shape = RoundedCornerShape(16.dp),
-            colors =
-                ButtonDefaults.outlinedButtonColors(
-                    contentColor = MaterialTheme.colorScheme.primary,
-                ),
-            modifier = Modifier.size(width = 100.dp, height = 40.dp),
-            elevation =
-                ButtonDefaults.elevation(
-                    defaultElevation = 8.dp,
-                    pressedElevation = 0.dp,
-                    disabledElevation = 0.dp,
-                ),
+    if (openDialogOne.value) {
+        val datePickerState = rememberDatePickerState()
+        val state = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+        val confirmEnabled = derivedStateOf { datePickerState.displayMode != null }
+
+        DatePickerDialog(
+            onDismissRequest = { openDialogOne.value = false },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openDialogOne.value = false
+                        var date = "No Selection"
+                        if (datePickerState.selectedDateMillis != null) {
+                            date = Tools.convertToTime(datePickerState.selectedDateMillis!!)
+                        }
+                        dateResult = date
+                    },
+                    enabled = confirmEnabled.value,
+                ) {
+                    Text(text = "Ok")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDialogOne.value = false
+                    },
+                ) {
+                    Text(text = "Cancel")
+                }
+            },
         ) {
-            Text(
-                text = book,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold,
-                style = MaterialTheme.typography.titleLarge,
-            )
-//
+            DatePicker(state = datePickerState)
         }
     }
-    if (openDialog.value) {
+}
+
+class Tools {
+    companion object {
+        fun openLink(
+            eContext: Context,
+            url: String,
+        ) {
+            val openUrl = Intent(Intent.ACTION_VIEW)
+            openUrl.data = Uri.parse(url)
+            eContext.startActivity(openUrl)
+        }
+
+        fun convertToTime(time: Long): String {
+            val date = Date(time)
+            val format = SimpleDateFormat("dd MMM YYYY")
+            return format.format(date)
+        }
     }
 }
