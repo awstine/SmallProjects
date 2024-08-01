5bc7030ffe50fd80e79c971db8af696c616cabb8
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-08-01 13:15:27 -0700

    initial commit

--- MyApplication/app/src/main/java/com/example/myapplication/Composables/GetStartedScreen.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Composables/GetStartedScreen.kt
@@ -3,25 +3,25 @@
 package com.example.myapplication.Composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -32,47 +32,40 @@ import com.example.myapplication.data.Screen
 @Composable
 @Suppress("ktlint:standard:function-naming")
 fun GetStartedScreen(navController: NavController) {
-    Surface(
-        color = MaterialTheme.colorScheme.tertiary,
-        contentColor = MaterialTheme.colorScheme.onBackground,
+    Box(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.saloon_background2),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize(),
+        )
         Column(
             modifier =
                 Modifier
                     .fillMaxSize()
-                    // .background(MaterialTheme.colorScheme.primary)
                     .padding(16.dp),
-            verticalArrangement = Arrangement.Center, // Center the content vertically
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                "Welcome to Our Saloon App",
+                text = "Welcome to Our Saloon App",
                 style = MaterialTheme.typography.headlineLarge,
-                textAlign = TextAlign.Center, // Center align text
-                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 32.dp),
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Image(
-                modifier =
-                    Modifier
-                        .fillMaxWidth(0.8f)
-                        .padding(bottom = 32.dp),
-                // Adjusted padding for a better layout
-                painter = painterResource(id = R.drawable.saloon),
-                contentDescription = null,
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
             ElevatedButton(
                 onClick = {
                     navController.navigate(Screen.HomeScreen.route)
                 },
                 modifier =
                     Modifier
-                        .fillMaxWidth(0.8f) // 80% of available width
-                        .padding(vertical = 16.dp)
-                        .size(60.dp),
+                        .fillMaxWidth(0.8f)
+                        .height(60.dp),
                 elevation =
                     ButtonDefaults.elevatedButtonElevation(
                         defaultElevation = 8.dp,
@@ -87,9 +80,32 @@ fun GetStartedScreen(navController: NavController) {
                 Text(
                     text = "Get Started",
                     style = MaterialTheme.typography.headlineSmall,
-                    textAlign = TextAlign.Center, // Center align text
+                    textAlign = TextAlign.Center,
                 )
             }
         }
     }
 }
+
+//    Surface(
+//        color = MaterialTheme.colorScheme.tertiary,
+//        contentColor = MaterialTheme.colorScheme.onBackground,
+//    ) {
+//        Box(
+//            modifier = Modifier.fillMaxSize(),
+//        ) {
+
+//            Spacer(modifier = Modifier.height(16.dp))
+//            Image(
+//                modifier = Modifier,
+//                painter = painterResource(id = R.drawable.saloon_background2),
+//                contentDescription = null,
+//                contentScale = ContentScale.FillBounds,
+//            )
+//
+//            Spacer(modifier = Modifier.height(16.dp))
+//
+//            }
+//        }
+//    }
+// }
