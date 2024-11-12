d2a604d4b79c94dd8875142a9f1522d61b2f961c
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-11-12 14:33:47 -0800

    Digimon Responce latest updates

--- Retrofit2/app/src/main/java/com/example/retrofit2/MainActivity.kt
+++ Retrofit2/app/src/main/java/com/example/retrofit2/MainActivity.kt
@@ -4,26 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.retrofit2.model.Digimon
 import com.example.retrofit2.ui.theme.Retrofit2Theme
+import com.example.retrofit2.ui.view.HomeScreen
+import com.example.retrofit2.ui.viewModel.DigimonViewModel
 
 class MainActivity : ComponentActivity() {
+
+    val digimonViewModel by viewModels<DigimonViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Retrofit2Theme {
-                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    DigimonList(digimonList = digimonViewModel.digimonListResponce)
+                    digimonViewModel.getDigimonList()
                 }
             }
         }
@@ -31,17 +42,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DigimonList(
+    digimonList: List<Digimon>
+) {
+ LazyColumn {
+     itemsIndexed(items = digimonList) { index, digimon ->
+        HomeScreen(digimon = digimon)
+     }
+ }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Retrofit2Theme {
-        Greeting("Android")
-    }
-}
---
+++ Retrofit2/app/src/main/java/com/example/retrofit2/ui/view/HomeScreen.kt
@@ -0,0 +1,90 @@
+package com.example.retrofit2.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.size.Scale
+import coil.transform.CircleCropTransformation
+import com.example.retrofit2.R
+import com.example.retrofit2.model.Digimon
+
+@Composable
+fun HomeScreen(digimon: Digimon) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp,4.dp)
+            .fillMaxWidth()
+            .height(110.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Surface {
+            Row (
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize()
+            ){
+                Image(
+                    painter = rememberImagePainter(data = digimon.imageUrl,
+                        builder = {
+                            scale(Scale.FILL)
+                            placeholder(R.drawable.nodata)
+                            transformations(CircleCropTransformation())
+                        }),
+                    contentDescription = "Digimon Image",
+                )
+
+                Column(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .weight(0.8f),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = digimon.name,
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier
+                            .background(Color.LightGray)
+                            .padding(4.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        text = digimon.level,
+                        style = MaterialTheme.typography.displayMedium,
+                        modifier = Modifier
+                            .background(Color.LightGray)
+                            .padding(4.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun View1() {
+    val digi = Digimon("Agumon", " ", "Rookie")
+    HomeScreen(digimon = digi)
+}
