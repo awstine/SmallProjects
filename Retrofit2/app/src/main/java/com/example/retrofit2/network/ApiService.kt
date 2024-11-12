699100bbf69a7584f4436702ec14521133ec7d07
Остин Джесси <116760268+awstine@users.noreply.github.com>
2024-11-12 14:29:01 -0800

    Digimon Responce latest updates

---
+++ Retrofit2/app/src/main/java/com/example/retrofit2/network/ApiService.kt
@@ -0,0 +1,25 @@
+package com.example.retrofit2.network
+
+import com.example.retrofit2.model.Digimon
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("moviesList.json")
+    suspend fun getDigimons(): List<Digimon>
+
+    // ApiService instance which interface is created by Retrofit
+    companion object{
+        var apiService: ApiService? = null
+        fun getInstance(): ApiService{
+            if(apiService == null){
+                apiService = Retrofit.Builder()
+                    .baseUrl("https://digimon-api.vercel.app/api/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(ApiService::class.java)
+            }
+            return apiService!!
+        }
+    }
+}
