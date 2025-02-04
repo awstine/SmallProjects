bd516e7b7aa5aca79c145f391573805749e27b2b
Остин Джесси <116760268+awstine@users.noreply.github.com>
2025-02-04 12:28:54 -0800

    working on american express ui (hutsy)

--- amerikanExpressUi/app/src/main/java/com/example/bankingui/ui/screen/LoginScreen.kt
+++ amerikanExpressUi/app/src/main/java/com/example/bankingui/ui/screen/LoginScreen.kt
@@ -3,14 +3,32 @@ package com.example.bankingui.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bankingui.R
 
 @Composable
 @Preview
 fun LoginScreen() {
-    Column(){
-        Image()
+    Column(
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center,
+    ){
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = null,
+        )
+        Spacer(Modifier.height(4.dp))
+
+        Text(
+            "Good Afternoon!"
+        )
 
     }
 }
